88a75e2a7b1f1b5d3ad8a911892313384b92c05a
Chaebum Kim <tw2cha@gmail.com>
2021-06-30 16:26:51 +0900

    Ch2 assignment1 - Web Server

---
+++ ch2-application-layer/web-server/HTTPServer.py
@@ -0,0 +1,37 @@
+
+from socket import *
+
+serverPort = 6789
+serverName = '127.0.0.1'
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((serverName, serverPort))
+serverSocket.listen(1)
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+
+        # Send one HTTP header line into socket
+        connectionSocket.send('HTTP/1.1 200 OK\r\n'.encode())
+        connectionSocket.send('\r\n'.encode())
+
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send('\r\n'.encode())
+
+        # Close client socket
+        connectionSocket.close()
+
+    except IOError:
+        # Send response message for file not found
+        connectionSocket.send('HTTP/1.1 400 Bad Request\r\n'.encode())
+
+        # Close client socket
+        connectionSocket.close()
